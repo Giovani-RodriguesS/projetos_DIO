1ec4651be7625a3800991ec0e66217c38425bfe9
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-05-06 13:46:52 -0300

    fix: syntax correction

--- projeto-estacionamento/obj/Debug/net8.0/projeto-estacionamento.AssemblyInfo.cs
+++ projeto-estacionamento/obj/Debug/net8.0/projeto-estacionamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto-estacionamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6297f7dd6ba267065093a17f1768da97621a7d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto-estacionamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto-estacionamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
