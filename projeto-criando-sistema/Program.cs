ba6d4ea52aafb9b547f08a70f7e5ccc6f653225f
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-05-08 19:48:02 -0300

    fix: 'desafio Criando um Sistema' completed

--- projeto-criando-sistema/Program.cs
+++ projeto-criando-sistema/Program.cs
@@ -3,12 +3,14 @@
 Nokia nokia = new Nokia("123432", "I5", "000000", 64);
 Iphone iphoneX = new Iphone("12345678", "X", "090909", 128);
 
+Console.WriteLine("Nokia: ");
 nokia.Ligar();
-iphoneX.Ligar();
-
 nokia.InstalarAplicativo("WhatsApp");
-iphoneX.InstalarAplicativo("Tinker");
-
 nokia.ReceberLigacao();
+
+Console.WriteLine("\nIphone: ");
+iphoneX.Ligar();
+iphoneX.InstalarAplicativo("Tinker");
 iphoneX.ReceberLigacao();
+
 
